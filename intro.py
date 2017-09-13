40123b6bff41e500d1dba770cac1093022829358
hanthana075 <31895850+hanthana075@users.noreply.github.com>
2017-09-13 00:01:28 -0400

    Add files via upload
    class program done on python

---
+++ intro.py
@@ -0,0 +1,43 @@
+###declaring variables
+class_number = 5300
+print "class number is: ", class_number
+class_name = "practical genomics"
+print "class name is: ", class_name
+students = ["Tom", "Sarah", "Anne", "Bob"] #list of strings
+print "students in the class are: ", students
+grades = [86, 90, 84, 95] #list of integers
+print "student grades are: ", grades
+info = (["Masters", 1], "phD", ["phD"], "Masters") #tuple
+print info
+
+students.index("Tim")
+
+###playing with lists
+print students[0]
+print students[-1]
+print students[-2]
+print students[-5]
+print students[::1]
+print students[::-1]
+print students[3:0:-1]
+print students[3::-1]
+
+index = 0
+while index < len(students):
+  print index
+  print students[index]
+  index = index + 1
+  
+for s in students:
+  print s
+
+a=range(10)
+a=range(1,10+1)
+a=range(2,10+1,2)
+
+###playing with strings
+student = "Sarah"
+print student
+print len(student)
+print student[0]
+print student[0]+student[1]+student[2]+student[3]+student[4]
